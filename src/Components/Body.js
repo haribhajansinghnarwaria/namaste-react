362cf9f785ad9ce4dd3a700eb4f934b61dd2af42
Haribhajan Singh Narwaria <haribhajan.narwaria@ad.infosys.com>
2024-06-29 22:27:31 +0530

    episode-5part-02

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,13 +1,26 @@
 import RestaurantCard from "./RestaurantCard";
 import { resList } from "../Utils/mockData";
+import { useState } from "react";
+
 const Body = () => {
+    const [ListOfRestaurant, setListOfRestaurant] = useState(resList);
     return(
         <div className = "body">
-            <div className = "search">search</div>
+            <div className = "filter">
+                <button className="filter-btn" 
+                onClick = {()=> {
+                    const filteredList = ListOfRestaurant.filter(
+               (res) => res.info.avgRating > 4
+                    );
+                    setListOfRestaurant(filteredList);
+                }
+            }
+                >Top Rated Restaurant</button>
+            </div>
             <div className = "res-container">
                 {/* <RestaurantCard resdata = {resList[0]} />
                 <RestaurantCard resdata = {resList[2]} /> */}
-                {resList.map((restaurant) => (
+                {ListOfRestaurant.map((restaurant) => (
                     <RestaurantCard key = {restaurant.info.id} resdata = {restaurant} />//unique key to avoid warning :each child in a list hould have a unique "key" prop 
                 ))}
             </div>
